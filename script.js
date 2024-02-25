01037ede6fd3ecf22586e781c9ae2899da6ae509
Abhinav Yadav <abhinav.codes01@gmail.com>
2024-02-25 09:56:13 +0530

    tRIDOSHA COMPLETED

---
+++ script.js
@@ -0,0 +1,648 @@
+const questions=[
+
+{    question: "Among your family members, is your complexion considered fairer?  ",
+answers:[
+    {text:"yes",type:"kapha",score:40},
+    {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Does your temperature of oral cavity remain towards upper limit of normal range?",
+answers:[
+   {text:"yes",type:"pitta",score:17},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Is your body undernourished/ emaciated?",
+answers:[
+   {text:"yes",type:"vata",score:30},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Do you think you have intense sexual desire?",
+answers:[
+    {text:"yes",type:"kapha",score:120},
+    {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you have excessive black moles, Freckles etc on your skin?",
+answers:[
+   {text:"no",type:"pitta",score:0},
+
+   {text:"yes",type:"pitta",score:17},
+]
+},
+{question: "Have you got rough, low, broken or obstructed voice?",
+answers:[
+   {text:"yes",type:"vata",score:30},
+   {text:"no",type:"vata",score:0},
+]
+},
+{questions: "Are you considered attractive among your friends? ",
+answers:[
+   {text:"yes",type:"kapha",score:40},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Have you experienced premature graying, wrinkling of skin & early baldness?",
+answers:[
+   {text:"yes",type:"pitta",score:17},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Do you change walking speed & style from time to time?",
+answers:[
+   {text:"yes",type:"vata",score:40},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Have you got well built muscles?  ",
+answers:[
+   {text:"yes",type:"kapha",score:60},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you have soft, scanty, brown hair on your face, body & head?",
+answers:[
+   {text:"yes",type:"pitta",score:17},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Do you keep changing your food habits from time to time?",
+answers:[
+   {text:"yes",type:"vata",score:40},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Do you think you have intense sexual desire?",
+answers:[
+   {text:"yes",type:"kapha",score:120},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you have soft, scanty, brown hair on your face, body & head?",
+answers:[
+   {text:"yes",type:"pitta",score:17},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Do you keep changingyour walking / jogging habit from time to time?",
+answers:[
+   {text:"yes",type:"vata",score:40},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Do you change your body posture frequently? (You cannot manage yourself in a stable posture for a long duration.) ?",
+answers:[
+   {text:"yes",type:"kapha",score:0},
+   {text:"no",type:"kapha",score:60},
+]
+},
+{question: "Do you involve yourself in risky & heroic activities requiring physical strength often?",
+answers:[
+   {text:"yes",type:"pitta",score:24},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Do you keep your joints, eyes, eyebrows, jaw, lips, tongue, head, Shoulder, hands & feet frequently moving?",
+answers:[
+   {text:"yes",type:"vata",score:120},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Do you have a well-nourished & normally developed body? (You are neither malnourished nor obese.) ?",
+answers:[
+   {text:"yes",type:"kapha",score:120},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you have ability to digest large quantities of food easily?",
+answers:[
+   {text:"yes",type:"pitta",score:24},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Are you considered a talkative among your friends?",
+answers:[
+   {text:"yes",type:"vata",score:60},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Are you lazy and disinterested in activities like morning walk/ jogging, swimming or any type of outdoor games) ?",
+answers:[
+   {text:"yes",type:"kapha",score:40},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you take large quantities of food & drink in comparison to others?",
+answers:[
+   {text:"yes",type:"pitta",score:24},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Do you have prominent veins & tendons all over the body?",
+answers:[
+   {text:"yes",type:"vata",score:60},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Are you slow in consuming the food?  (Even after all have left the dining hall, you are still consuming the same amount of food)?",
+answers:[
+   {text:"yes",type:"kapha",score:40},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you get easily irritated for small/negligible problem in day-to-day life?",
+answers:[
+   {text:"yes",type:"pitta",score:24},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Do you generally start the work assigned to you immediately?",
+answers:[
+   {text:"yes",type:"vata",score:15},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: " When you go to morning walk or college or office, do you walk slowly in comparison to others?",
+answers:[
+   {text:"yes",type:"kapha",score:40},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you consume food more frequently than others? (5-6 times/day)?",
+answers:[
+   {text:"yes",type:"pitta",score:24},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Does Your sleep last less than 6 hours per day? Or Can your sleep be disturbed easily?",
+answers:[
+   {text:"yes",type:"vata",score:30},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "If you are assigned any work, do you take some extra time to start it?",
+answers:[
+   {text:"yes",type:"kapha",score:40},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you get irritated easily? (E.g., when you do not get breakfast on time in your hostel or when the power goes off while watching a cricket match or your favourite movie over television)?",
+answers:[
+   {text:"yes",type:"vata",score:15},
+   {text:"no",type:"vata",score:0},
+]
+},
+
+{question: "Do you get irritated easily? (For example, when you don’t get breakfast on time in your hostel or when the power goes off while watching a cricket match or your favourite movie on television)?",
+answers:[
+   {text:"yes",type:"kapha",score:0},
+   {text:"no",type:"kapha",score:40},
+]
+},
+{question: "Do you have soft & loose muscle bulk especially around the joints?",
+answers:[
+   {text:"yes",type:"pitta",score:60},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Do you get frightened easily?",
+answers:[
+   {text:"yes",type:"vata",score:15},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Are you late to develop/suffer from symptoms after exposure to common causative factors? (For example, during seasonal changes, when your friends are easily caught up with flu etc., you are still healthy among them)?",
+answers:[
+   {text:"yes",type:"kapha",score:40},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "In comparison to others do you pass urine & stool in large quantities and do you perspire more?",
+answers:[
+   {text:"yes",type:"pitta",score:60},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Do you make friends easily & also lose them easily?",
+answers:[
+   {text:"yes",type:"vata",score:15},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Does your gait (style of walking) change with respect to or manner frequently?",
+answers:[
+   {text:"yes",type:"kapha",score:0},
+   {text:"no",type:"kapha",score:120},
+]
+},
+{question: "Do your friends complain of bad smell being emitted from your body & mouth?",
+answers:[
+   {text:"yes",type:"pitta",score:120},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Do you generally learn things quickly?",
+answers:[
+   {text:"yes",type:"vata",score:15},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Do you feel hungry more frequently and do you consume more food in comparison to others?",
+answers:[
+   {text:"yes",type:"kapha",score:0},
+   {text:"no",type:"kapha",score:30},
+]
+},
+{question: "Do you think you have intense sexual desire?",
+answers:[
+   {text:"yes",type:"pitta",score:0},
+   {text:"no",type:"pitta",score:120},
+]
+},
+{question: "Do you prefer hot/warm drinks over cold drinks?",
+answers:[
+   {text:"yes",type:"vata",score:15},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Do you tolerate heat easily?",
+answers:[
+   {text:"yes",type:"kapha",score:30},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you have cracks on the body especially on the heels?",
+answers:[
+{text:"yes",type:"vata",score:60},
+{text:"no",type:"vata",score:0},
+]
+},
+{question: "Do you consume liquids in more quantity and frequency in comparison to others?",
+answers:[
+   {text:"yes",type:"kapha",score:0},
+   {text:"no",type:"kapha",score:30},
+]
+},
+{question: "Do you feel excessive hunger & thirst in comparison to others?",
+answers:[
+   {text:"yes",type:"pitta",score:17},
+   {text:"no",type:"pitta",score:0},
+]
+},
+{question: "Is your long term memory weak? (E.g., you can remember only few names of your friends at your primary school)?",
+answers:[
+   {text:"yes",type:"vata",score:15},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Do you perspire less in comparison to others?",
+answers:[
+   {text:"yes",type:"kapha",score:30},
+   {text:"no",type:"kapha",score:0},
+]
+},{question: "Among your family members, is your complexion considered fairer?",
+answers:[
+{text:"yes",type:"pitta",score:17},
+{text:"no",type:"pitta",score:0},
+]
+},
+{question: "Are your nails, teeth, hands, feet and hairs on your body and face rough?",
+answers:[
+   {text:"yes",type:"vata",score:120},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Are sounds produced frequently in your joints on movement ?",
+answers:[
+   {text:"yes",type:"kapha",score:0},
+   {text:"no",type:"kapha",score:120},
+]
+},
+{question: "Do you generally develop symptoms immediately after exposure to common causative factors?(You are easily caught by diseases like flu, allergy during seasonal changes)?",
+answers:[
+   {text:"yes",type:"vata",score:15},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Have you got a good/ attractive complexion ?",
+answers:[
+   {text:"yes",type:"kapha",score:60},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you shiver in winter season more than your friends?",
+answers:[
+   {text:"yes",type:"vata",score:60},
+   {text:"no",type:"vata",score:0},
+]
+},
+{question: "Have you got sweet & pleasant voice?",
+answers:[
+   {text:"yes",type:"kapha",score:60},
+   {text:"no",type:"kapha",score:0},
+]
+},
+{question: "Do you often feel stiffness in your body after exercise, travelling?",
+answers:[
+   {text:"yes",type:"vata",score:60},
+   {text:"no",type:"vata",score:0},
+]
+}, 
+
+
+{question: "Are some crackling sounds produced in your joints during movements?",
+answers:[
+   {text:"yes",type:"vata",score:60},
+   {text:"no",type:"vata",score:0},
+]
+}
+];
+
+
+
+function updatekProgressBar(progressBar, value) {
+    value = Math.round(value);
+    progressBar.querySelector(".progress__fillk").style.width = `${value}%`;
+    progressBar.querySelector(".progress__text").textContent = `${value}%`;
+  }
+  
+  function updatepProgressBar(progressBar, value) {
+    value = Math.round(value);
+    progressBar.querySelector(".progress__fillp").style.width = `${value}%`;
+    progressBar.querySelector(".progress__text").textContent = `${value}%`;
+  }
+  function updatevProgressBar(progressBar, value) {
+    value = Math.round(value);
+    progressBar.querySelector(".progress__fillv").style.width = `${value}%`;
+    progressBar.querySelector(".progress__text").textContent = `${value}%`;
+  }
+   myProgresskBar = document.querySelector(".progressk");
+   myProgresspBar = document.querySelector(".progressp");
+   myProgressvBar = document.querySelector(".progressv");
+  
+  /* Example */
+
+
+  const next = document.createElement('div');
+  next.className =  'next';
+  next.textContent = "Start new assisment";
+  next.addEventListener('click',askQuestion);
+
+
+let counter=0;
+ let randomNumber;
+ const start=document.getElementById('start');
+
+ start.addEventListener('click',askQuestion);
+ 
+ const chatMessages = document.getElementById('chat-messages');
+ var kscore=0;
+   var  pscore=0;
+    var vscore=0;
+    var tkscore=0,tpscore=0,tvscore=0;
+    var arr=[]
+    for(var i=0;i<questions.length;i++)
+    {
+        arr[i]=i;
+    }
+   
+   
+
+    function getRandomQuestion(arr) {
+    // Check if the array is not empty
+    if (arr.length > 0) {
+        // Generate a random index
+        const randomIndex = Math.floor(Math.random() * arr.length);
+
+        // Get the random number at the generated index
+         randomNumber = arr[randomIndex];
+
+        // Remove the selected number from the array
+        arr.splice(randomIndex, 1);
+
+        // Return the unique random number
+        return randomNumber
+    } else {
+        // If the array is empty, return null or handle as needed
+        return null;
+    }
+}
+
+    
+
+    function appendMessage(message) {
+        counter++;
+        
+       const messageElement = document.createElement('div');
+        messageElement.className =  'bot-message';
+        messageElement.textContent = message;
+        chatMessages.appendChild(messageElement);
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+  
+    function askQuestion() {
+       
+      
+  
+start.disabled="true";
+start.display="none";
+
+
+
+
+
+
+       
+
+        const currentQuestion = getRandomQuestion(arr);
+        appendMessage(`Dr Ayurveda: ${questions[currentQuestion].question}`);
+        appendans(randomNumber);
+    }
+    function appendans(randomNumber)
+    {
+        questions[randomNumber].answers.forEach(answer => {
+        const button =document.createElement("button");
+        button.innerHTML=answer.text;
+        button.classList.add("btn");
+        chatMessages.appendChild(button);
+       
+        
+           
+            button.dataset.type=answer.type;
+            button.dataset.score=answer.score;
+        
+            button.addEventListener('click',selectanswer);
+        var temp=answer.type;
+        if(temp=="kapha")
+    {
+        tkscore+=parseInt(answer.score);
+
+    }
+    if(temp=="pitta")
+    {
+        tpscore+=parseInt(answer.score);
+    }
+
+    if(temp=="vata")
+    {
+        tvscore+=parseInt(answer.score);
+    }
+});
+
+    }
+    function selectanswer(e)
+    {
+        const selectbtn=e.target;
+        selectbtn.style.background="skyblue";
+        const type=selectbtn.dataset.type;
+    const score=parseInt(selectbtn.dataset.score);
+        if(type=="kapha")
+        {
+            kscore+=score;
+        }
+        if(type=="pitta")
+        {
+            pscore+=score;
+        }
+        if(type=="vata")
+        {
+            vscore+=score;
+        }
+        Array.from(chatMessages.children).forEach(buton=>{
+    
+            buton.disabled=true;})
+            var v=0,k=0,p=0;
+             k=parseInt(Math.floor((kscore/tkscore)*100));
+             p=parseInt(Math.floor((pscore/tpscore)*100));
+             v=parseInt(Math.floor((vscore/tvscore)*100));
+             if(isNaN(k))
+             {
+                k=0;
+             }
+             if(isNaN(v))
+             {
+                v=0;
+             }
+             
+             if(isNaN(p))
+             {
+                p=0;
+             }
+             
+            updatekProgressBar(myProgresskBar, k);
+            updatepProgressBar(myProgresspBar, p);
+            updatevProgressBar(myProgressvBar, v); 
+
+
+
+
+        const currentQuestion = getRandomQuestion(arr);
+        if(currentQuestion==null)
+        {
+            appendMessage(`Kappa   : ${k}%`);
+            appendMessage("Vata   : "+v+"%");
+            appendMessage("Pitaa   : "+p+"%");
+            chatMessages.appendChild(next);
+            
+           
+            start.innerHTML="Start New Assisment";
+            //reset();
+            start.disabled=false;
+
+
+        }
+        if(counter>10)
+        {
+            if(k>75)
+            {
+            appendMessage(` you are diagnosied with kapha dosha with ${k}%`);
+            //reset();
+            chatMessages.appendChild(next);
+            //reset();
+            start.disabled=false;
+
+            }
+            else  if(p>75)
+            {
+            appendMessage(` you are diagnosied with pitta dosha with ${p}%`);
+           // reset();
+            chatMessages.appendChild(next);
+            tkscore=0;tvscore=0;tpscore=0;
+            kscore=0;vscore=0;pscore=0;
+            k=0;v=0;
+            p=0;
+            
+            start.disabled=false;
+            }
+            else if(v>75)
+            {
+            appendMessage(` you are diagnosied with vata dosha with ${v}%`);
+            //reset();
+            chatMessages.appendChild(next);
+            tkscore=0;tvscore=0;tpscore=0;
+            kscore=0;vscore=0;pscore=0;
+            k=0;v=0;
+            p=0;
+            
+           
+            start.disabled=false;
+            }
+            else{
+                appendMessage(`Dr Ayurveda: ${questions[currentQuestion].question}`);
+                appendans(randomNumber);
+                }
+                tkscore=0;tvscore=0;tpscore=0;
+                kscore=0;vscore=0;pscore=0;
+                k=0;v=0;
+                p=0;
+                
+
+        }
+      else{
+        appendMessage(`Dr Ayurveda: ${questions[currentQuestion].question}`);
+        appendans(randomNumber);
+        }
+    }
+        function reset()
+{
+
+
+    for(var i=0;i<questions.length;i++)
+    {
+        arr[i]=i;
+    }
+    updatekProgressBar(myProgresskBar, 0);
+    updatepProgressBar(myProgresspBar, 0);
+    updatevProgressBar(myProgressvBar, 0); 
+    start.innerHTML="Start the Assisment";
+   while(chatMessages.firstChild)
+   {
+    chatMessages.removeChild(chatMessages.firstChild);
+   } 
+}
+
+
+
+
+
+
+
+
+/*function togglePopup() {
+    var popup = document.getElementById("chat-messages");
+    popup.style.display = (popup.style.display === "block") ? "none" : "block";
+}
+
+function closePopup() {
+    var popup = document.getElementById("chat-messages");
+    popup.style.display = "none";
+} */    
+
+       
+        
+
+
+
+    
+
